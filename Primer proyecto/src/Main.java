bd7026b890006bb7b6f62e383038dc3d5a6cd917
nomadwebs <programacion@nomadwebs.com>
2023-02-17 09:10:51 +0100

    Initial commit

---
+++ Primer proyecto/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //La meva primera linea de codi Java
+        System.out.println("1. Hello world!");
+        System.out.println("2. Això mola molt");
+
+        if (1 == 1) {
+            System.out.printf("3. Son Iguals");
+        }else{
+            System.out.println("3. no es possible");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 1. Curso introducción a la programación Java
+ Para subir las actividades del curso de introducción a la programación
