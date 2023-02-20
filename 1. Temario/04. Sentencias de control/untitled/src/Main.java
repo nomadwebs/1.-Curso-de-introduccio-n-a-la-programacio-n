a4200a8887711f4c19cccc1181e641a2d527e676
nomadwebs <programacion@nomadwebs.com>
2023-02-21 00:40:21 +0100

    Teoria + ejercicio 4

--- 1. Temario/04. Sentencias de control/untitled/src/Main.java
+++ 1. Temario/04. Sentencias de control/untitled/src/Main.java
@@ -31,6 +31,7 @@ public class Main {
 
         //Este bucle avalua antes de entrar en el bucle. En caso de no cumplir la condición ya no entra.
         int contador = 10;
+        System.out.println("Aquí mostramos el WHILE");
         while (contador > 0){
             System.out.println(contador);
             contador--;
@@ -39,10 +40,82 @@ public class Main {
         //Para este caso, como mínimo entrará una vez en el bucle y evaluará al final para ver
         //si se sigue cumpliendo la condición
         int contador2 = 10;
+        System.out.println("Aquí mostramos el DO WHILE");
         do {
             System.out.println(contador2);
             contador2--;
         } while (contador2 > 0);
+
+
+        //3. El siguiente ejemplo es para el bucle FOR, que es un híbrido entre los dos anteriores
+        //Y el que se usará en la mayoría de ocasiones
+        // Los utilizaremos muchas veces para recorrer arrays
+        System.out.println("Aquí mostramos el FOR");
+        for (i=0; i<=10; i++){
+            System.out.println(i);
+        }
+
+        //Los bloques de inicio y final del for son opcionales. Pero queda un poco feote
+        int temperatura = 15;
+        for (; temperatura != 15;){
+            System.out.println(temperatura);
+        }
+
+
+        //La mayoría de las veces utilizaremos los fors para recorrer arrays
+        int valores[] = new int[5];  //Aquí tenemos una declaración de un array
+        int valores2[] = {10, 20, 30, 40, 50}; //También lo podemos crear e inicialiciar directamente así.
+
+
+        for (int y = 0; y < valores2.length; y++){
+            System.out.println(valores2[y]);
+        }
+
+        /*
+        int infinita = 0;
+        for (;;){   //Bucle infinito. Esto dará vueltas hasta que algo pete
+            System.out.println(infinita);
+            infinita++;
+        }
+        */
+
+
+
+        //4. Switch Case --> Teoria de los interruptores
+        // Es super importante utilizar el Break para frenar la elección en la que nos hemos parado
+        var Estacion = "Verano";
+
+        switch (Estacion){
+            case "Verano":
+                System.out.println("Es verano");
+                break; //El break es para salir de la condición.
+                       //Sin el Break el programa continuará evaluando todos los casos
+            case "Invierno":
+                System.out.println("Es Invierno y hace frío");
+                break;
+            default:
+                System.out.println("Otra estación");
+        }
+
+        //Esto también se podría mostrar de la siguiente manera
+
+        var hoy_es = "Martes";
+
+        switch (hoy_es){
+            case "Lunes":
+            case "Martes":
+            case "Miércoles":
+            case "Jueves":
+            case "Viernes":
+                System.out.println("Hoy es laborable");
+                break;
+            case "Sábado":
+            case "Domingo":
+                System.out.println("Hoy NO es laborable");
+                break;
+
+        }
+
     }
 
 }
--- 2. Ejercicios/Ejercicios tema 4/src/Main.java
+++ 2. Ejercicios/Ejercicios tema 4/src/Main.java
@@ -1,5 +1,80 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1. Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        int numeroIf = 7;
+
+        if (numeroIf > 0){
+            System.out.println("Es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("Es negativo");
+        }else {
+            System.out.println("Es 0");
+        }
+        System.out.println("-------------------------------------");
+
+        //2. Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3, el bloque de código que tendrá el bucle deberá:
+        //
+        //Incrementar el valor de la variable en uno cada vez que se ejecute.
+        //
+        //Mostrarlo por pantalla cada vez que se ejecute.
+
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+        System.out.println("-------------------------------------");
+
+
+        //3. Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+        int numeroWhile2 = 0;
+
+        do {
+            System.out.println(numeroWhile2);
+            numeroWhile2 = numeroWhile2 + 3;
+        } while (numeroWhile2 < 3);
+        System.out.println("-------------------------------------");
+
+        //4. Para el bucle For, crea una variable numeroFor, esta variable tendrá
+        // como valor 0 y su condición será que la variable sea igual o menor que 3,
+        // se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+
+        int numeroFor = 0;
+        for (numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+        System.out.println("-------------------------------------");
+
+        //5. Por último, para el Switch, deberás crear la variable estacion, y distintos case
+        // para las cuatro estaciones del año. Dependiendo del valor de la variable estacion
+        //se deberá mandar un mensaje por consola informando de la estación en la que está.
+        // También habrá que poner un default para cuando el valor de la variable no sea una estación.
+
+        var estacion = "Otoñ7o";
+
+        switch (estacion){
+            case "Verano":
+                System.out.println("Es verano y la gente se pone el bañador");
+                break; //El break es para salir de la condición.
+            //Sin el Break el programa continuará evaluando todos los casos
+            case "Invierno":
+                System.out.println("Es Invierno y hace frío");
+                break;
+            case "Otoño":
+                System.out.println("Es otoño y caen las hojas");
+                break;
+            case "Primavera":
+                System.out.println("Es primavera y la sangre altera");
+                break;
+            default:
+                System.out.println("El valor introducido no es una estación");
+        }
+        System.out.println("-------------------------------------");
+        System.out.println("Fin del ejercicio 4");
+
+
+
     }
 }
