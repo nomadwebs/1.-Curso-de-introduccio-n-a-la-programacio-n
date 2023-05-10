762d5721cd701f0260bad2528aba4ec1f690b8e1
nomadwebs <programacion@nomadwebs.com>
2023-05-10 22:37:19 +0200

    jkjkjkj

---
+++ 1. Temario/10. Métodos de clase/metodos-clases/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Métodos de clases");
+        System.out.println("-----------------");
+    }
+}
+
+//Los métodos de clases son como funciones
+
+//CLASE A
+    //PROPIEDAD 1
+    //PROPIEDAD 2
+
+    //PRIVADO [NOMBRE_DEL_METODO] (PARÁMETROS) [VALORES]
+
+//Según el lenguaje los métodos se pueden implementar de una forma u otra.
+
+//¡¡¡Un método es una función dentro de una clase!!!
+//--------------------------------------------------
+
+/*
+
+
+
+
+
+ */
